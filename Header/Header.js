22e4d6030b6660a26e74591a5b95dee07682ff44
san-geetha-s <101059528+san-geetha-s@users.noreply.github.com>
2022-10-10 19:12:46 +0530

    header folder uploaded

---
+++ Header/Header.js
@@ -0,0 +1,20 @@
+import React ,{Component} from 'react'
+
+
+
+
+import './Header.css'
+import Tabs from '../Components/Tabs';
+import { Tab } from 'react-bootstrap';
+import TabList from '@mui/joy/TabList';
+function Header() {
+  return (
+    <div className='header'>
+     <h1 className='title' >Date with doggyy....</h1>
+     
+    </div>
+    
+  )
+}
+
+export default Header
